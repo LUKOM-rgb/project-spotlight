047d160dba2452a3a2cd422e0c765466ac470c6d
Sérgio Daniel Gomes Gonçalves <sergiodaniel6@gmail.com>
2026-05-11 18:07:00 +0100

    conexao a base de dados feita

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,24 @@
-require('dotenv').config();
-console.log("Loaded ENV config:");
+import dotenv from 'dotenv';
+dotenv.config();
 
-const express = require('express');
-
-// read environment variables from .env file
-
-const app = express();
-const port = process.env.PORT;	// use environment variables
-const host = process.env.HOST;
+import { Sequelize } from 'sequelize';
 
 
-app.use(express.json());
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
+  }
+);
+
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+  process.exit(1);
+}
