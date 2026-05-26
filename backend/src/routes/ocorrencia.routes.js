588de83b8a4730f9ac457b3ee95c65f443e59c1e
LUKOM-rgb <40240224@esmad.ipp.pt>
2026-05-26 15:27:40 +0100

    fix routes

--- backend/src/routes/ocorrencia.routes.js
+++ backend/src/routes/ocorrencia.routes.js
@@ -5,16 +5,16 @@ import {
   getAllOcorrencias,
   getOcorrenciaById,
   updateEstadoOcorrencia,
-  deleteOcorrencia
+  deleteOcorrencia,
 } from '../controllers/ocorrencia.controller.js'
 
 const router = express.Router()
 
 // Apenas administradores podem ver, alterar, adicionar e eliminar relatórios de ocorrências
-router.post('/', verifyToken, isAdmin, createOcorrencia)
-router.get('/', verifyToken, isAdmin, getAllOcorrencias)
-router.get('/:id', verifyToken, isAdmin, getOcorrenciaById)
-router.patch('/:id/', verifyToken, isAdmin, updateEstadoOcorrencia)
-router.delete('/:id', verifyToken, isAdmin, deleteOcorrencia)
+router.post('/', createOcorrencia)
+router.get('/', getAllOcorrencias)
+router.get('/:id', getOcorrenciaById)
+router.patch('/:id/', updateEstadoOcorrencia)
+router.delete('/:id', deleteOcorrencia)
 
 export default router
